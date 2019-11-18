a7282130648ec46ff10db5be0a3bad81242e6e8f
Andy Blackledge <andyblackledge@hotmail.com>
2019-11-18 21:15:42 +0000

    AB#78 Created a HelloFlowR example and updated the readme

---
+++ FlowR/Samples/HelloFlowR/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using FlowR;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelloFlowR
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceProvider =
+                new ServiceCollection()
+                    .AddMediatR(typeof(SayHelloRequest).Assembly)
+                    .BuildServiceProvider();
+
+            var mediator = serviceProvider.GetService<IMediator>();
+
+            var response = await mediator.Send(new SayHelloRequest { Name = "FlowR" });
+
+            Console.WriteLine($"response.Text: {response.OutputtedText}");
+            Console.WriteLine($"response.Trace: {response.Trace}");
+        }
+    }
+}
