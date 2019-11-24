f99c7f82eab61dcf79369e866403a4cecb6a1745
Andy Blackledge <andyblackledge@hotmail.com>
2019-11-24 19:24:02 +0000

    Added flow discovery test

--- FlowR/Samples/TwentyQuestions/TwentyQuestionsConsole/Program.cs
+++ FlowR/Samples/TwentyQuestions/TwentyQuestionsConsole/Program.cs
@@ -16,44 +16,23 @@ namespace TwentyQuestions
             var serviceCollection =
                 new ServiceCollection()
                     .AddMediatR(typeof(TwentyQuestionsRequest).Assembly)
-                    .AddMediatR(typeof(FlowDiscoveryRequest).Assembly)
                     .AddLogging(builder => { builder.AddConsole(); })
                     .Configure<LoggerFilterOptions>(options => options.MinLevel = LogLevel.Debug)
                     .AddTransient(typeof(IFlowLogger<>), typeof(CoreFlowLogger<>))
                     .AddSingleton<IConsoleService>(new ConsoleService());
 
-            FlowDiscovery.RegisterFlowTypes(typeof(TwentyQuestionsRequest).Assembly,
-                (intType, impType) => serviceCollection.AddSingleton(intType, impType));
-
             using (var serviceProvider = serviceCollection.BuildServiceProvider())
             {
                 var mediator = serviceProvider.GetService<IMediator>();
 
-                if (args.Length == 0)
-                {
-                    var response =
-                        mediator.Send(new TwentyQuestionsRequest())
-                            .GetAwaiter().GetResult();
+                var response =
+                    mediator.Send(new TwentyQuestionsRequest())
+                        .GetAwaiter().GetResult();
 
-                    Console.WriteLine("****************************************************");
-                    Console.WriteLine($"Guess: {response.Guess}");
-                    Console.WriteLine($"Trace: {response.Trace}");
-                    Console.WriteLine("****************************************************");
-                }
-                else
-                {
-                    var flowDiscoveryResponse =
-                        mediator.Send(new FlowDiscoveryRequest())
-                            .GetAwaiter().GetResult();
-
-                    var twentyQuestionsFlow =
-                        flowDiscoveryResponse.Flows.First(f => 
-                            f.Request.RequestType == typeof(TwentyQuestionsRequest));
-
-                    Console.WriteLine("****************************************************");
-                    Console.WriteLine(twentyQuestionsFlow.GetDotNotation());
-                    Console.WriteLine("****************************************************");
-                }
+                Console.WriteLine("****************************************************");
+                Console.WriteLine($"Guess: {response.Guess}");
+                Console.WriteLine($"Trace: {response.Trace}");
+                Console.WriteLine("****************************************************");
             }
         }
     }
